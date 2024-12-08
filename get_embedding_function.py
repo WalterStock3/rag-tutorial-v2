e0eb959ca8c848b7ab06ecc9dbe697bbe27ec3bf
Walter Stock 3 <walter.stock@gmail.com>
2024-12-08 15:47:04 -0500

    Base working version.

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,10 +1,7 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_community.embeddings.bedrock import BedrockEmbeddings
 
+from langchain_ollama import OllamaEmbeddings
 
 def get_embedding_function():
-    embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )
-    # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    # Use OllamaEmbeddings to populate the embeddings variable
+    embeddings = OllamaEmbeddings(model="llama3.3")
     return embeddings
